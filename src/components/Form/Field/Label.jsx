886819d3be95d73e8d682a7412b9e108093bd18e
Andy Wardley <abw@wardley.org>
2022-07-06 10:54:18 +0100

    Added labelClass to form labels

--- src/components/Form/Field/Label.jsx
+++ src/components/Form/Field/Label.jsx
@@ -3,7 +3,7 @@ import Optional from './Optional'
 import Required from './Required'
 
 export const Label = ({field}) =>
-  <label htmlFor={field.id}>
+  <label htmlFor={field.id} className={field.labelClass || ''}>
     {field.label||<span>&nbsp;</span>}
     {field.showRequired && field.required && <Required field={field} />}
     {field.showOptional && (! field.required) && <Optional field={field} />}
