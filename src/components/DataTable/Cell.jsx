3645c3a4f6e1d7d6674bd8da5525acccc0f6997d
Andy Wardley <abw@wardley.org>
2022-07-14 08:59:25 +0100

    Added onClick handler to DataTable cell

--- src/components/DataTable/Cell.jsx
+++ src/components/DataTable/Cell.jsx
@@ -3,14 +3,14 @@ import { propClasses } from '../../utils'
 import displayTypes from './Display'
 
 export const Cell = ({
-  row, column, name, field, value, rowClick,
+  row, column, name, field, value, rowClick, onClick,
   sortColumn
 }) => {
     const type    = column.type    || 'text';
     const Display = column.display || displayTypes[type] || displayTypes.default;
     const sorting = sortColumn === name;
     const classes = propClasses({ sorting }, type, column.bodyClass, column.className);
-    return <td className={classes} onClick={rowClick ? () => rowClick(row) : () => null}>
+    return <td className={classes} onClick={onClick || (rowClick ? () => rowClick(row) : () => null)}>
       <Display row={row} column={column} name={name} field={field} value={value}/>
     </td>
 }
