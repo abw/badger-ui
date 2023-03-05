c55c480e9a9f2ad8268bb72a07ed335e50a0d34b
Andy Wardley <abw@wardley.org>
2023-03-05 13:29:27 +0000

    New icons component for use with svg-icon-librarian

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,63 +1,201 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { init } from '../../config/iconLibrary'
+import icons from '../../config/icons.js'
+import { classNames, Themed } from '../../utils'
+import { isString } from '@abw/badger-utils';
 
-// initialise the custom icons configuration to load icons into library
-init();
+const faProps = {
+  fixedWidth: 'fa-fw',
+  spin: ' fa-spin',
+}
 
-const styles = {
-  solid: 'fas',
-  outline: 'far',
-  regular: 'far'
-};
+export const Icon = ({icons, transform, ...props}) => {
+  // icon can be specified as "icon" or "name" property and can be a string
+  // like "badger spin" where anything after the first icon name is added as
+  // a fa-XXX class
+  const spec = props.icon || props.name;
+  const [name, ...flags] = spec.split(' ');
+  const cflags = flags
+    .map( f => f.match(/^fa-/) ? f : `fa-${f}` )
+    .join(' ');
 
-export const Icon = (props) => {
-  let myProps = { ...props };
+  // lookup the icon in the icons library
+  const icon = icons[name];
 
-  // allow "name" as an alias for "icon" property
-  let icon = myProps.icon || myProps.name;
-  myProps.icon = icon;
-  delete myProps.name;
-
-  if (myProps.icon.match(/\bspin\b/)) {
-    myProps.spin = true;
-    myProps.icon = myProps.icon.replace(/\b\s*spin\b/, '');
+  if (! icon) {
+    console.log(`Invalid icon name:${name}`);
+    return null;
   }
-  if (myProps.icon.match(/\bflip-h\b/)) {
-    myProps.flip = 'horizontal';
-    myProps.icon = myProps.icon.replace(/\b\s*flip-h\b/, '');
-  }
+  // pull out the pertinent data
+  const { minx, miny, width, height, style } = icon;
+  const className = classNames(props, faProps);
 
-  // look for solid/outline/regular props
-  Object.keys(styles).forEach(
-    style => {
-      if (myProps[style]) {
-        delete myProps[style];
-        myProps.icon = [styles[style], icon];
+  return (
+    <svg
+      aria-hidden="true" focusable="false"
+      className={`svg-inline--fa icon ${className} ${cflags}`}
+      role="img" xmlns="http://www.w3.org/2000/svg"
+      viewBox={`${minx||0} ${miny||0} ${width} ${height}`}
+      style={style && ReactStyle(style)}
+    >
+      { transform
+        ? <Transform icon={icon} transform={transform}>
+            <IconPaths name={name} icon={icon}/>
+          </Transform>
+        : <IconPaths name={name} icon={icon}/>
       }
-    }
-  );
+    </svg>
+  )
+}
 
-  myProps.className = (myProps.className || '') + ' icon';
-
-  if (myProps.color) {
-    myProps.className += ' ' + myProps.color;
-    delete myProps.color;
+const IconPaths = ({icon}) => {
+  const {paths, path} = icon;
+  if (paths) {
+    return paths.map( (path, n) => <Path path={path} key={n}/> )
   }
-  // console.log('Icon props: ', myProps);
-  return <FontAwesomeIcon {...myProps} />
+  if (path) {
+    return <Path path={path}/>
+  }
+  console.log('No path or paths for icon');
+}
+
+const Path = ({path}) =>
+  isString(path)
+    ? <path fill="currentColor" d={path}/>
+    : <PathWithAttrs path={path}/>
+
+const PathWithAttrs = ({path}) => {
+  const {d, opacity=null, style=null} = path;
+  return <path
+    d={d}
+    fill="currentColor"
+    opacity={opacity}
+    style={style && ReactStyle(style)}
+  />
+}
+
+function ReactStyle(style) {
+  // e.g. fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round
+  // converted to object with React camel case: { fillRule: 'evenodd', etc }
+  return [ ...style.matchAll(/\s*([\w-]*?)\s*:\s*(.*?)(;|$)/g)]
+    .map( match => match.slice(1, 3) )
+    .reduce(
+      (hash, [key, value]) => {
+        hash[key.replace(/-./g, x => x[1].toUpperCase())] = value;
+        return hash;
+      },
+      { }
+    );
+}
+
+const Transform = ({icon, transform, children}) => {
+  const { width, height } = icon;
+  const halfw = width / 2;
+  const halfh = height / 2;
+  const trans = parseTransform(transform);
+  const tx    = trans.x * width  / 16;
+  const ty    = trans.y * height / 16;
+  const sx    = trans.size / 16 * (trans.flipX ? -1 : 1);
+  const sy    = trans.size / 16 * (trans.flipY ? -1 : 1);
+  const rt    = trans.rotate;
+  /*
+  if (transform) {
+    console.log('transform: ', transform);
+    console.log('trans: ', trans);
+    console.log(`tx:${tx}  ty:${ty}  sx:${sx}  sx:${sx}  rt:${rt}`);
+  }
+  */
+
+  return (
+    <g transform={`translate(${halfw} ${halfh})`}>
+      <g transform={`translate(${tx}, ${ty}) scale(${sx}, ${sy}) rotate(${rt} 0 0)`}>
+        <g transform={`translate(-${halfw} -${halfh})`}>
+          {children}
+        </g>
+      </g>
+    </g>
+  )
+}
+
+// Slightly modified version of:
+// https://github.com/FortAwesome/Font-Awesome/blob/6.x/js-packages/%40fortawesome/fontawesome-svg-core/index.js#L2706
+function parseTransform(transformString) {
+  var transform = {
+    size: 16,
+    x: 0,
+    y: 0,
+    flipX: false,
+    flipY: false,
+    rotate: 0
+  };
+  return transformString.toLowerCase().split(' ').reduce(
+    (acc, n) => {
+      const parts = n.toLowerCase().split('-');
+      const first = parts[0];
+      const rest = parts.slice(1).join('-');
+
+      if (first && rest === 'h') {
+        acc.flipX = true;
+        return acc;
+      }
+
+      if (first && rest === 'v') {
+        acc.flipY = true;
+        return acc;
+      }
+
+      const num = parseFloat(rest);
+
+      if (isNaN(num)) {
+        return acc;
+      }
+
+      switch (first) {
+        case 'grow':
+          acc.size = acc.size + num;
+          break;
+
+        case 'shrink':
+          acc.size = acc.size - num;
+          break;
+
+        case 'left':
+          acc.x = acc.x - num;
+          break;
+
+        case 'right':
+          acc.x = acc.x + num;
+          break;
+
+        case 'up':
+          acc.y = acc.y - num;
+          break;
+
+        case 'down':
+          acc.y = acc.y + num;
+          break;
+
+        case 'rotate':
+          acc.rotate = acc.rotate + num;
+          break;
+      }
+
+      return acc;
+    },
+    transform
+  );
 }
 
 Icon.propTypes = {
   /** The name of the icon. */
   name: PropTypes.string,
-  /** Explicitly indicate a solid icon (default). */
-  solid: PropTypes.bool,
-  /** Indicate a regular icon. */
-  regular: PropTypes.bool,
   /** Icon size. */
   size: PropTypes.oneOf(['xs', 'sm', 'lg', '1x', '2x', '3x', '4x', '5x', '6x', '7x', '8x', '9x', '10x']),
 };
 
-export default Icon;
+
+Icon.defaultProps = {
+  icons
+};
+
+export default Themed(Icon, 'Icon')
--- stories/components/Button.stories.jsx
+++ stories/components/Button.stories.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { Button, Buttons } from "../../src/components/Button";
+import React from 'react';
+import { Button, Buttons } from '../../src/components/Button';
 import { colors } from '../../src/config/colors'
 import { sizes } from '../../src/config/sizes'
 import { iconNames } from '../../src/config/iconNames'
 
 export default {
-  title: "Components/Button",
+  title: 'Components/Button',
   component: Button,
   args: {
     text: 'Hello World',
@@ -72,37 +72,55 @@ export const SolidButtons = () =>
 
 export const IconButtons = () =>
   <>
+    <p className="mar-t-none pad-b-2">
+      Use the <code className="code">icon</code> property for an icon-only
+      button, <code className="code">iconLeft</code> for an icon on the left
+      and/or <code className="code">iconRight</code> for an icon on the right.
+      You can add animation modifiers like <code className="code">flip</code>,{' '}
+      <code className="code">bounce</code> and <code className="code">spin</code>{' '}
+      after the icon name.
+    </p>
     <Button icon="warning" color="red" />
     <Button iconLeft="warning" color="green" text="warning" />
     <Button iconRight="warning" color="blue" text="warning" />
     <Button solid icon="warning" color="red" />
-    <Button solid iconLeft="warning" color="green" text="warning" />
-    <Button solid iconRight="warning" color="blue" text="warning" />
+    <Button solid iconLeft="warning flip" color="orange" text="warning" />
+    <Button solid iconRight="warning bounce" color="orange" text="warning" />
+    <Button solid iconRight="radiation spin" color="green" text="Saving"/>
   </>
 
 export const ButtonSets = () =>
   <>
+    <p className="mar-t-none pad-b-2">
+      Use the <code className="code">Buttons</code> component with an
+      array of <code className="code">buttons</code> to create a button set.
+    </p>
     <Buttons buttons={[
-      { iconLeft: "arrow-left", color: "red", text: "Left" },
-      { icon: "arrow-up", color: "green" },
-      { iconRight: "arrow-right", color: "blue", text: "Right" }
+      { iconLeft: 'arrow-left', color: 'red', text: 'Left' },
+      { icon: 'arrow-up', color: 'green' },
+      { iconRight: 'arrow-right', color: 'blue', text: 'Right' }
     ]} />
     <br />
     <Buttons buttons={[
-      { iconLeft: "arrow-left", color: "red", text: "Left", solid: true },
-      { icon: "arrow-up", color: "green" },
-      { iconRight: "arrow-right", color: "blue", text: "Right", solid: true }
+      { iconLeft: 'arrow-left', color: 'red', text: 'Left', solid: true },
+      { icon: 'arrow-up', color: 'green' },
+      { iconRight: 'arrow-right', color: 'blue', text: 'Right', solid: true }
     ]} />
     <br />
     <Buttons solid buttons={[
-      { iconLeft: "arrow-left", color: "red", text: "Left" },
-      { icon: "arrow-up", color: "green" },
-      { iconRight: "arrow-right", color: "blue", text: "Right" }
+      { iconLeft: 'arrow-left', color: 'red', text: 'Left' },
+      { icon: 'arrow-up', color: 'green' },
+      { iconRight: 'arrow-right', color: 'blue', text: 'Right' }
     ]} />
   </>
 
 export const DisabledButton = () =>
   <>
+    <p className="mar-t-none pad-b-2">
+      Add the <code className="code">disabled</code> property to disable
+      a button
+    </p>
+
     <Button
       iconLeft="arrow-left" color="red" text="Left" disabled={true}
     />
