ebafedb08f45cfe03076be327a13dee0e884f5ae
Andy Wardley <abw@wardley.org>
2023-03-24 17:45:15 +0000

    New icons

--- .storybook/BadgerTheme.js
+++ .storybook/BadgerTheme.js
@@ -4,6 +4,6 @@ export default create({
   base: 'light',
   brandTitle: 'badger-ui Storybook',
   brandUrl: 'https://github.com/abw/badger-ui',
-  brandImage: "badger-ui.svg",
+  brandImage: "badger-ui-2.svg",
   brandTarget: '_self',
 });
