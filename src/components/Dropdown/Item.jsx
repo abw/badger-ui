8cc8bac2f0457a39d7f7ec6fa75e339db4e13830
Andy Wardley <abw@wardley.org>
2023-03-28 08:44:18 +0100

    Dropdown cleanup

--- src/components/Dropdown/Item.jsx
+++ src/components/Dropdown/Item.jsx
@@ -2,31 +2,39 @@ import React from 'react'
 import Link from './Link.jsx'
 import { Content } from './Content.jsx'
 
-export const Item = (props) => props.disabled
-  ? <div className={'item disabled'} aria-disabled={true}>
+export const Item = (props) => {
+  if (props.disabled) {
+    return (
+      <div className={'item disabled'} aria-disabled={true}>
+        <Content {...props}/>
+      </div>
+    )
+  }
+  if (props.to) {
+    return (
+      <Link {...props} className="item" onClick={() => props.setClicked(1)}/>
+    )
+  }
+  if (props.href) {
+    return (
+      <a href={props.href} target={props.target} label={props.label} className="item" onClick={() => props.setClicked(1)}>
+        <Content {...props}/>
+      </a>
+    )
+  }
+  return (
+    <span
+      className="item"
+      onClick={
+        () => {
+          props.setClicked(1);
+          props.onClick()
+        }
+      }
+    >
       <Content {...props}/>
-    </div>
-  : props.to ?
-      <Link {...props} onClick={() => props.setClicked(1)}/>
-    /*
-    <Link to={props.to||'/'} label={props.label||props.text} className="item" onClick={() => props.setClicked(1)}>
-      <Content {...props}/>
-    </Link>
-    */
-    : props.href ?
-        <a href={props.href} target={props.target} label={props.label} className="item" onClick={() => props.setClicked(1)}>
-          <Content {...props}/>
-        </a>
-      : <span
-          className="item"
-          onClick={
-            () => {
-              props.setClicked(1);
-              props.onClick()
-            }
-          }
-        >
-          <Content {...props}/>
-        </span>
+    </span>
+  )
+}
 
 export default Item
--- stories/components/Dropdown.stories.jsx
+++ stories/components/Dropdown.stories.jsx
@@ -24,19 +24,41 @@ export const SimpleDropdown = () =>
   </Dropdown>
 
 export const OutlineDropdownMenu = () =>
-  <DropdownMenu
-    size="small" text="Menu"
-    iconLeft="bars" color="brown"
-    className="mar-b-12"
-    items={[
-      { text: 'User',        iconLeft: 'user' },
-      { text: 'Company',     iconLeft: 'users' },
-      { separator: true },
-      { text: 'Radiation',   iconLeft: 'radiation' },
-      { separator: true },
-      { text: 'Disabled',    iconLeft: 'times', disabled: true },
-    ]}
-  />
+  <>
+    <p className="mar-t-none">
+      The <code className="code">DropdownMenu</code> component allows you
+      to define an array of <code className="code">items</code> to appear in
+      a menu.  Each can contain the <code className="code">text</code> to
+      display and <code className="code">iconLeft</code> and/or{' '}
+      <code className="code">iconRight</code> to display an icon on either
+      side.  Add the <code className="code">href</code> property to create a
+      regular link, the <code className="code">to</code> property to create
+      a <code className="code">Link</code> component (which by default uses
+      React Router v6), or an <code className="code">onClick</code> handler.
+    </p>
+    <p>
+      Add the <code className="code">disabled</code> property to disable an
+      option.
+    </p>
+    <p>
+      For separators, define an item
+      with a <code className="code">separator</code> property set
+      to <code className="code">true</code>.
+    </p>
+    <DropdownMenu
+      size="small" text="Menu"
+      iconLeft="bars" color="brown"
+      className="mar-b-16"
+      items={[
+        { text: 'User',        iconLeft: 'user' },
+        { text: 'Company',     iconLeft: 'users' },
+        { separator: true },
+        { text: 'Radiation',   iconLeft: 'radiation' },
+        { separator: true },
+        { text: 'Disabled',    iconLeft: 'times', disabled: true },
+      ]}
+    />
+  </>
 
 export const SolidDropdownMenu = () =>
   <DropdownMenu
@@ -52,38 +74,55 @@ export const SolidDropdownMenu = () =>
   />
 
 export const RightAlignedMenu = () =>
-  <DropdownMenu
-    size="small" text="Actions" className="float-right mar-b-12" right
-    iconLeft="cog" color="orange" solid
-    items={[
-      { text: 'Activate',
-        iconLeft: 'cog', iconLeftColor: 'green',
-        onClick: () => window.alert('You clicked on \'Activate\'')
-      },
-      { text: 'Eliminate',
-        iconLeft: 'trash', iconLeftColor: 'red',
-        onClick: () => window.alert('You clicked on \'Eliminate\'')
-      },
-      { text: 'Add a User',
-        iconLeft: 'user', iconLeftColor: 'blue',
-        iconRight: 'plus', iconRightColor: 'green', iconRightClass: 'small',
-        onClick: () => window.alert('You clicked on \'Add a User\'')
-      },
-    ]}
-  />
+  <>
+    <p className="mar-t-none">
+      Add the <code className="code">right</code> property to have the
+      menu items right-aligned. Note that you&apos;ll still have to implement
+      your own alignment if you want the menu to appear on the right of the
+      container, e.g. by adding <code className="code">text-right</code> to the
+      container or <code className="code">float-right</code> in the {' '}
+      <code className="code">className</code> for the <code className="code">DropdownMenu</code>.
+    </p>
+    <DropdownMenu
+      size="small" text="Actions" className="float-right mar-b-12" right
+      iconLeft="cog" color="orange" solid
+      items={[
+        { text: 'Activate',
+          iconLeft: 'cog', iconLeftColor: 'green',
+          onClick: () => window.alert('You clicked on \'Activate\'')
+        },
+        { text: 'Eliminate',
+          iconLeft: 'trash', iconLeftColor: 'red',
+          onClick: () => window.alert('You clicked on \'Eliminate\'')
+        },
+        { text: 'Add a User',
+          iconLeft: 'user', iconLeftColor: 'blue',
+          iconRight: 'plus', iconRightColor: 'green', iconRightClass: 'small',
+          onClick: () => window.alert('You clicked on \'Add a User\'')
+        },
+      ]}
+    />
+  </>
 
 export const CustomTrigger = () => {
   const Trigger = () => <Icon name="bars"/>
 
-  return <DropdownMenu
-    size="small" className="mar-b-12"
-    Trigger={Trigger}
-    items={[
-      { text: 'Foo', onClick: () => window.alert('You clicked on \'Foo\'') },
-      { text: 'Bar', onClick: () => window.alert('You clicked on \'Bar\'') },
-      { text: 'baz', onClick: () => window.alert('You clicked on \'Baz\'') },
-    ]}
-  />
+  return (
+    <>
+      <p className="mar-t-none">
+        You can define your own trigger using the <code className="code">Trigger</code> property.
+      </p>
+      <DropdownMenu
+        size="small" className="mar-b-12"
+        Trigger={Trigger}
+        items={[
+          { text: 'Foo', onClick: () => window.alert('You clicked on \'Foo\'') },
+          { text: 'Bar', onClick: () => window.alert('You clicked on \'Bar\'') },
+          { text: 'baz', onClick: () => window.alert('You clicked on \'Baz\'') },
+        ]}
+      />
+    </>
+  )
 }
 
 export const ClickToLockOpen = () =>
